36d2126f9e41d12b595777890854f868e29d851b
Emily Foley <foleyemilym@gmail.com>
2014-06-14 14:00:37 -0700

    continuing version control, rails issue rendering json

--- subreddit2/app/controllers/pages_controller.rb
+++ subreddit2/app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
 
   def index
-    # @subreddit = params[:subreddit]
+    @subreddit = params[:subreddit]
   end
 
   def get_subreddits
-  
+    respond_to do |format|
+      format.js { render :json => {cat: "meow"} }
+    end
   end
 
 end
