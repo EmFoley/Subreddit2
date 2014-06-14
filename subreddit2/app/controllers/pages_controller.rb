e0a6500c29d46a9251ddd59a47f187d4c8d2475b
Emily Foley <foleyemilym@gmail.com>
2014-06-13 18:23:25 -0700

    indexing into reddit array

--- subreddit2/app/controllers/pages_controller.rb
+++ subreddit2/app/controllers/pages_controller.rb
@@ -3,9 +3,12 @@ class PagesController < ApplicationController
   def index
     reddit = Snoo::Client.new
     subreddit = params[:subreddit]
+    require 'debugger'
+    debugger
     @list_array = reddit.get_listing({subreddit: subreddit, page: 'new', limit: 20})['data']['children']
-    @list_array.to_json
+    title_1 = @list_array[0]["data"]["title"]
+    url_1 = @list_array[0]["data"]["url"]
+    permalink_1 = @list_array[0]["data"]["permalink"]
   end
-  @list_array
 
 end
